5935bde185c119c9c50113192fdc0f9175a19ad3
Josh Myers <joshuajmyers@gmail.com>
2018-01-31 13:49:22 +0000

    Rename test.groovy now we have a thing working

--- src/jobs/dcs.groovy
+++ src/jobs/dcs.groovy
@@ -1,15 +1,8 @@
-import com.jenkins.hmpo.builders.BaseJobBuilder
 import com.jenkins.hmpo.builders.MavenJobBuilder
 
-new BaseJobBuilder()
-    .name("foo")
-    .description("foo")
-    .build(this)
-
 new MavenJobBuilder()
-    .name('bar')
-    .description('bar job')
-    .goals('clean test')
+    .name('dcs-dap-submission-worker-high')
+    .goals('-U clean package  -Punit')
     .gitProject('hmpo-badger')
     .gitRepository('ilikebadgers')
     .gitUrl('git.com.badgers.net')
