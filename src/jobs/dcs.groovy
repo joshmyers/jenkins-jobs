7dc97f31d9cb7741a21fdd9b3c8b58d119cff8a3
Josh Myers <joshuajmyers@gmail.com>
2018-01-31 14:23:31 +0000

    Moar jobs!

--- src/jobs/dcs.groovy
+++ src/jobs/dcs.groovy
@@ -4,3 +4,31 @@ new MavenJobBuilder()
     .name('dcs-dap-submission-worker-high')
     .goals('-U clean package  -Punit')
     .build(this)
+
+new MavenJobBuilder()
+    .name('dcs-rest-referee-api-low')
+    .build(this)
+
+new MavenJobBuilder()
+    .name('dcs-rest-referee-dwp-api-low')
+    .build(this)
+
+new MavenJobBuilder()
+    .name('dcs-rest-referee-mi-api-high')
+    .build(this)
+
+new MavenJobBuilder()
+    .name('dcs-rest-referee-verify-low')
+    .build(this)
+
+new MavenJobBuilder()
+    .name('dcs-soap-atos-payment-stub-low')
+    .build(this)
+
+new MavenJobBuilder()
+    .name('dcs-soap-dwp-cis-stub-low')
+    .build(this)
+
+new MavenJobBuilder()
+    .name('or-rest-oab-low')
+    .build(this)
