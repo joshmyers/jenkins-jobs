4d8f48e25358bb0c6ae1e62bcc033eb20058eebb
Josh Myers <joshuajmyers@gmail.com>
2018-01-31 12:28:34 +0000

    Add MavenJob Builder

--- src/jobs/test.groovy
+++ src/jobs/test.groovy
@@ -1,6 +1,18 @@
 import com.jenkins.hmpo.builders.BaseJobBuilder
+import com.jenkins.hmpo.builders.MavenJobBuilder
 
 new BaseJobBuilder()
     .name("foo")
     .description("foo")
+    .build(this)
+
+new MavenJobBuilder()
+    .name('bar')
+    .description('bar job')
+    .goals('clean test')
+    .gitProject('hmpo-badger')
+    .gitRepository('ilikebadgers')
+    .gitUrl('git.com.badgers.net')
+    .gitlabPush(true)
+    .promote(true)
     .build(this)
---
+++ src/main/groovy/com/jenkins/hmpo/builders/MavenJobBuilder.groovy
@@ -0,0 +1,96 @@
+package com.jenkins.hmpo.builders
+
+import groovy.transform.builder.Builder
+import groovy.transform.builder.SimpleStrategy
+import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.Job
+import javaposse.jobdsl.dsl.jobs.MavenJob
+import com.jenkins.hmpo.utils.CommonUtils
+
+/**
+ * MavenJob Builder
+ *
+ * <p>
+ *      creates a mavenJob with colorized input,
+ *      log rotator, email notifications and build claiming
+ * </p>
+ * @param name used to name the job
+ * @param description job description
+ * @param goals Maven goals to build
+ * @param branchToBuild Branch to check out for SCM
+ * @param gitProject Git project for SCM
+ * @param gitRepository Git repoistory for SCM
+ * @param gitUrl Git URL for SCM
+ * @param gitlabPush trigger on Gitlab Push to repo (boolean)
+ * @param promote should this job promote builds (boolean)
+ * @param emails list of developer to get notifications
+ */
+
+@Builder(builderStrategy = SimpleStrategy, prefix = '')
+class MavenJobBuilder {
+    String name
+    String description
+    String goals = 'clean test'
+    String branchToBuild = 'master'
+    String gitProject = 'hmpo-pex'
+    String gitRepository
+    String gitUrl = 'git.com.hmpo.net'
+    Boolean gitlabPush = false
+    Boolean promote = false
+    List<String> emails = []
+
+    MavenJob build(DslFactory dslFactory) {
+        dslFactory.mavenJob(name) {
+            gitRepository = gitRepository ?: name
+            it.description this.description
+            CommonUtils.addDefaults(delegate)
+            publishers {
+                if (emails) {
+                    publishers {
+                        CommonUtils.addExtendedEmail(delegate, emails)
+                    }
+                }
+            }
+            scm {
+                git {
+                    extensions {
+                        wipeOutWorkspace()
+                    }
+                    remote {
+                       url("git@${gitUrl}:${gitProject}/${gitRepository}.git")
+                    }
+                    branch(branchToBuild)
+                }
+                goals(goals)
+            }
+            if (gitlabPush) {
+                triggers {
+                    gitlabPush {
+                        buildOnMergeRequestEvents(true)
+                        buildOnPushEvents(true)
+                        enableCiSkip(true)
+                        setBuildDescription(false)
+                        rebuildOpenMergeRequest('source')
+                        commentTrigger('rebuild')
+                        skipWorkInProgressMergeRequest(false)
+                    }
+                }
+            }
+            if (promote) {
+                properties{
+                    promotions {
+                        promotion {
+                            name('int')
+                            conditions {
+                                manual('tester')
+                            }
+                            actions {
+                                shell('echo foo;')
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
